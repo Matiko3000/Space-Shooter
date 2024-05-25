58039e555d2025645ff45644e0f59b50fb7ae919
Matiko3000 <matiko3000@gmail.com>
2024-05-26 01:34:37 +0200

    Update AudioPlayer.cs

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -6,7 +6,7 @@ public class AudioPlayer : MonoBehaviour
 {
     [Header("Shooting")]
     [SerializeField] AudioClip shootingClip;
-    [SerializeField] [Range(0f, 1f)] float shootingVolume = 1f;
+    [SerializeField] [Range(0f, 1f)] float shootingVolume = 1f;//too loud
 
     [Header("Taking Damage")]
     [SerializeField] AudioClip damageTakenClip;
